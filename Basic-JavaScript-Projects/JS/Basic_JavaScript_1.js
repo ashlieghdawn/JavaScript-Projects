e78dcf2026b37aa96a6b23ebe347eaac1ea0a1a9
ashlieghdawn <ashlieghdawn@icloud.com>
2020-03-28 14:04:47 -0700

    commit new changes to js code

---
+++ Basic-JavaScript-Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+var Family="The Johnsons", Mom="Ronnie", Dad="Todd", Sister="Shelli";
+var Family=Family.fontcolor("Blue");
+var Mom=Mom.fontcolor("Red");
+var Dad=Dad.fontcolor("Green");
+var Sister=Sister.fontcolor("Purple");
+document.write(Sister);
+
+
+document.write(4+4);
